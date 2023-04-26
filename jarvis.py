fd307ccf48574205d71ced33f5291cc7035cd0e3
christian <christianostoni82@gmail.com>
2023-04-26 20:04:43 +0200

    risolti problemi relativi ai permessi windows, e modificata la versione della libreria playsound: 1.2.2

--- jarvis.py
+++ jarvis.py
@@ -3,6 +3,7 @@ import openai
 import json
 from gtts import gTTS
 from playsound import playsound
+import os
 
 class assistente:
     def __init__(self, api_key):
@@ -13,13 +14,16 @@ class assistente:
         self.messaggio = messaggio
 
     def riconoscimento(self):
+        print("cosa vuoi chiedere a Jarvis?")
         recognizer_istance = sr.Recognizer()
         with sr.Microphone() as source:
-            recognizer_istance.adjust_for_ambient_noise(source)
+            recognizer_istance.adjust_for_ambient_noise(source, duration=1)
+            print("calibrato correttamente")
             audio = recognizer_istance.listen(source)
         
         self.text = recognizer_istance.recognize_google(audio, language="it-IT")
         print("testo riconosciuto: " + self.text)  
+        print("INVIO A CHATGPT.....")
 
     def chatGpt(self):
         openai.api_key= self.api_key
@@ -35,6 +39,10 @@ class assistente:
         self.riconosciuto = True
 
     def text_to_speech(self):
+        cwd = os.getcwd()
+        file = "audio.mp3"
+        indirizzo = os.path.join(cwd, file)
+        os.remove(indirizzo)
         tts = gTTS(text=self.messaggio, lang="it")
         tts.save("audio.mp3")
         playsound("audio.mp3")
